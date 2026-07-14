fea38066eb7eef1a335b2ee8d3232987a33171d3
PerThoyra <per.thoyra@outlook.com>
2026-07-14 14:34:29 +0200

    added route grouping for route versions

--- go-db-gorm/pkg/routes/item-routes.go
+++ go-db-gorm/pkg/routes/item-routes.go
@@ -8,12 +8,17 @@ import (
 )
 
 var RegisterItemRoutes = func(c *gin.Engine) {
-
-	c.GET("/item/", controllers.GetAllTodoItems)
-	c.GET("/item/{itemId}", controllers.GetTodoItemById)
-	c.POST("/item/", controllers.CreateItem)
-	c.PUT("/item/{itemId}", controllers.UpdateItem)
-	c.DELETE("/item/{itemId}", controllers.DeleteItem)
+	// Simple group: v1
+	// Should result in routes looking like '../v1/item/'
+	// Add more groups below this if needed, ie v2, v3 etc
+	{
+		v1 := c.Group("/v1")
+		v1.GET("/item/", controllers.GetAllTodoItems)
+		v1.GET("/item/{itemId}", controllers.GetTodoItemById)
+		v1.POST("/item/", controllers.CreateItem)
+		v1.PUT("/item/{itemId}", controllers.UpdateItem)
+		v1.DELETE("/item/{itemId}", controllers.DeleteItem)
+	}
 }
 
 func init() {
