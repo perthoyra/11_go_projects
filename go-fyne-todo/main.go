4b696b5f2d4663f95d6130ac4ffb8869367c3ad6
PerThoyra <per.thoyra@outlook.com>
2026-07-29 17:51:28 +0200

    fyne update

--- go-fyne-todo/main.go
+++ go-fyne-todo/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Printf("Heelu World!")
+	a := app.New()
+	w := a.NewWindow("TODO App")
+
+	w.SetContent(widget.NewLabel("TODOs will go here"))
+	w.ShowAndRun()
 }
